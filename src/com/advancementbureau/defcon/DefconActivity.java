0abf62addcab7ff9db45b0b825f213b3cec92598
Adriano Bianchini <animationbureau@gmail.com>
2012-03-27 12:07:20 -0700

    Long press stuff

--- src/com/advancementbureau/defcon/DefconActivity.java
+++ src/com/advancementbureau/defcon/DefconActivity.java
@@ -16,6 +16,8 @@ public class DefconActivity extends SuperDefconActivity {
 	
 	SharedPreferences mGameSettings;
 	public boolean firstBootDone;
+	public int currentDefcon;
+	public int infoDefcon;
 	
     /** Called when the activity is first created. */
     @Override
@@ -24,26 +26,82 @@ public class DefconActivity extends SuperDefconActivity {
         setContentView(R.layout.main);
         SharedPreferences bootPref = getSharedPreferences(FIRST_BOOT, MODE_PRIVATE);
         SharedPreferences.Editor editor = bootPref.edit();
+        if (mGameSettings.contains(DEFCON)) {
+			currentDefcon = mGameSettings.getInt(DEFCON, 0);
+		}
         if (bootPref.getBoolean(FIRST_BOOT, true)) {
         	editor.putBoolean("boot", firstBootDone);
             editor.commit();
-            PopUp(); 
+            PopUp(R.string.pop_notif, R.string.pop_notif_info); 
             }
-        LinearLayout defOne = (LinearLayout) findViewById(R.id.LinearLayout_DefconOne);
         
+        LinearLayout defOne = (LinearLayout) findViewById(R.id.LinearLayout_DefconOne);
         defOne.setOnClickListener(new View.OnClickListener() {
         	public void onClick(View view) {
-        		toastIt();
+        		storeIt(1);
+        	}
+        });
+        defOne.setOnLongClickListener(new View.OnLongClickListener() {
+        	public boolean onLongClick(View view) {
+        		PopUp(R.string.defcon_one_pop, R.string.defcon_one_info);
+        		return true;
+        	}
+        });
+        
+        LinearLayout defTwo = (LinearLayout) findViewById(R.id.LinearLayout_DefconTwo);
+        defTwo.setOnClickListener(new View.OnClickListener() {
+        	public void onClick(View view) {
+        		storeIt(2);
+        	}
+        });
+        defTwo.setOnLongClickListener(new View.OnLongClickListener() {
+        	public boolean onLongClick(View view) {
+        		PopUp(R.string.defcon_two_pop, R.string.defcon_two_info);
+        		return true;
+        	}
+        });
+        
+        LinearLayout defThree = (LinearLayout) findViewById(R.id.LinearLayout_DefconThree);
+        defThree.setOnClickListener(new View.OnClickListener() {
+        	public void onClick(View view) {
+        		storeIt(3);
+        	}
+        });
+        defThree.setOnLongClickListener(new View.OnLongClickListener() {
+        	public boolean onLongClick(View view) {
+        		PopUp(R.string.defcon_three_pop, R.string.defcon_three_info);
+        		return true;
+        	}
+        });
+        
+        LinearLayout defFour = (LinearLayout) findViewById(R.id.LinearLayout_DefconFour);
+        defFour.setOnClickListener(new View.OnClickListener() {
+        	public void onClick(View view) {
+        		storeIt(4);
+        	}
+        });
+        defFour.setOnLongClickListener(new View.OnLongClickListener() {
+        	public boolean onLongClick(View view) {
+        		PopUp(R.string.defcon_four_pop, R.string.defcon_four_info);
+        		return true;
+        	}
+        });
+        
+        LinearLayout defFive = (LinearLayout) findViewById(R.id.LinearLayout_DefconFive);
+        defFive.setOnClickListener(new View.OnClickListener() {
+        	public void onClick(View view) {
+        		storeIt(5);
+        	}
+        });
+        defFive.setOnLongClickListener(new View.OnLongClickListener() {
+        	public boolean onLongClick(View view) {
+        		PopUp(R.string.defcon_five_pop, R.string.defcon_five_info);
+        		return true;
         	}
         });
         
     }
     
-    
-    
-    /*public void onClickDefconOne(View view) {
-    	Toast.makeText(this, "click", 2000).show();
-    }*/
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -60,14 +118,22 @@ public class DefconActivity extends SuperDefconActivity {
     	return true;
     }
     
-    public void toastIt() {
-    	Toast.makeText(this, "click", 2000).show();
+    public void storeIt(int i) {
+    	Toast.makeText(this, "Defcon " + i, 2000).show();
+    	SharedPreferences defPref = getSharedPreferences(DEFCON, MODE_PRIVATE);
+        SharedPreferences.Editor defEditor = defPref.edit();
+    	defEditor.putInt("boot", i);
+        defEditor.commit();
     }
     
-    public void PopUp(){
+    /*
+     * Used to make a simple dialog box
+     * Requires int id of a title string and message string
+     */
+    public void PopUp(int title, int message){
         new AlertDialog.Builder(this)
-        .setTitle(R.string.pop_notif)
-        .setMessage(R.string.pop_notif_info)
+        .setTitle(title)
+        .setMessage(message)
         .setPositiveButton("Close", new OnClickListener() {
             public void onClick(DialogInterface arg0, int arg1) {
             	firstBootDone = false;
